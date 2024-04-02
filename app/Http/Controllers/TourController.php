1d7216861a9315dad45a8e61ab662a8a3c9cc0ca
EDU-NOVINBIN-Ali-Vaghey <a.vaghey0@gmail.com>
2024-04-02 10:09:38 +0330

    Implemented the base of the TourController.

--- app/Http/Controllers/TourController.php
+++ app/Http/Controllers/TourController.php
@@ -6,5 +6,43 @@ use Illuminate\Http\Request;
 
 class TourController extends Controller
 {
-    //
+    /**
+     * Create new tour.
+     */
+    public function create(Request $request)
+    {
+        //TODO
+    }
+
+    /**
+     * Get the information of a tour.
+     */
+    public function read()
+    {
+        //TODO
+    }
+
+    /**
+     * Update a tour.
+     */
+    public function update()
+    {
+        //TODO
+    }
+
+    /**
+     * Delete a tour.
+     */
+    public function delete()
+    {
+        //TODO
+    }
+
+    /**
+     * Update or make Certificates for tour.
+     */
+    public function updateCertificate()
+    {
+        //TODO
+    }
 }
--- routes/user/agency.php
+++ routes/user/agency.php
@@ -3,11 +3,19 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\AgencyInfoController;
+use App\Http\Controllers\TourController;
 
 Route::middleware(['auth:sanctum', 'isAgency'])->prefix('agency/')->group(function () {
 
     //------------------------- Profile Info -------------------------
     Route::get('info', [AgencyInfoController::class, 'getInfo']);
     Route::put('info', [AgencyInfoController::class, 'updateOrMake']);
+
+    //------------------------ Tour Management -----------------------
+    Route::post('tour', [TourController::class, 'create']);
+    Route::get('tour/{id}', [TourController::class, 'read']);
+    Route::put('tour/{id}', [TourController::class, 'update']);
+    Route::delete('tour/{id}', [TourController::class, 'delete']);
+    Route::put('tour/{id}/certificates', [TourController::class, 'updateCertificate']);
 
 });
