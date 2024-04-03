231ac93b690597ea943be9d66ec7719ed59bb164
EDU-NOVINBIN-Ali-Vaghey <a.vaghey0@gmail.com>
2024-04-03 14:29:42 +0330

    Implemented the bases of Hotel Model.

---
+++ app/Models/Hotel.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\AsCollection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Hotel extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'admin_id',
+        'name',
+        'address',
+        'country',
+        'state',
+        'city',
+        'gallery',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'gallery' => AsCollection::class,
+        ];
+    }
+}
