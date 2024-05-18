767ee990add8ad0503e91a26d92ca3bbeb007112
EDU-NOVINBIN-Ali-Vaghey <a.vaghey0@gmail.com>
2024-05-18 13:52:28 +0330

    Cors enabled.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,12 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->use([
             \App\Http\Middleware\CORSMiddleware::class,
+            \Illuminate\Http\Middleware\TrustProxies::class,
+            \Illuminate\Http\Middleware\HandleCors::class,
+            \Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance::class,
+            \Illuminate\Http\Middleware\ValidatePostSize::class,
+            \Illuminate\Foundation\Http\Middleware\TrimStrings::class,
+            \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         ]);
         
         $middleware->api(prepend: [
