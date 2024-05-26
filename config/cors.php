6c4d4a664bdc0cef232d51384e9a8f5d0f06f414
Ali Vaghey <a.vaghey0@gmail.com>
2024-05-26 21:21:15 +0330

    dsfasdfaf

--- config/cors.php
+++ config/cors.php
@@ -23,7 +23,7 @@ return [
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['x-xsrf-token', 'x-requested-with'],
+    'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
