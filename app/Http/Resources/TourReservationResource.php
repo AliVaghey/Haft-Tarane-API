5ae7c9cbef0a8743e2be1fd435d94e2c78719358
EDU-NOVINBIN-Ali-Vaghey <a.vaghey0@gmail.com>
2024-06-23 13:30:38 +0330

    limited the reservation

--- app/Http/Resources/TourReservationResource.php
+++ app/Http/Resources/TourReservationResource.php
@@ -14,6 +14,45 @@ class TourReservationResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        $tour = $this->tour;
+        $support = $tour->support;
+        $hotel = $this->hotel;
+        return [
+            'total_price' => $this->total_price,
+            'passengers_count' => $this->passengers_count,
+            'passengers' => $this->passengers,
+            'status' => $this->status,
+            'tour' => [
+                'id' => $tour->id,
+                'title' => $tour->title,
+                'support' => [
+                    'name' => $support->name,
+                    'phone' => $support->phone,
+                ],
+                'trip_type' => $tour->trip_type,
+                'tour_styles' => $tour->tour_styles,
+                'evening_support' => $tour->evening_support,
+                'midnight_support' => $tour->midnight_support,
+                'origin' => $tour->origin,
+                'destination' => $tour->destination,
+                'staying_nights' => $tour->staying_nights,
+            ],
+            'agency' => [
+                'id' => $tour->agency->id,
+                'name' => $tour->agency->name,
+            ],
+            'date' => [
+                'id' => $this->date->id,
+                'start' => $this->date->start,
+                'end' => $this->date->end,
+            ],
+            'hotel' => [
+                'id' => $hotel->id,
+                'name' => $hotel->name,
+                'address' => $hotel->address,
+                'stars' => $hotel->stars,
+                'photo' => $hotel->firstPhotoUrl(),
+            ],
+        ];
     }
 }
