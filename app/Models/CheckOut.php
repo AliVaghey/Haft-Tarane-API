1671ab51b90cc21b0cfa6c3a12b93de21e1a70f7
EDU-NOVINBIN-Ali-Vaghey <a.vaghey0@gmail.com>
2024-07-03 09:27:29 +0330

    set up the model files

---
+++ app/Models/CheckOut.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class CheckOut extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'agency_id',
+        'admin_id',
+        'total_price',
+        'profit',
+        'real_price',
+        'receipt_photo'
+    ];
+
+    public function agency(): BelongsTo
+    {
+        return $this->belongsTo(AgencyInfo::class, 'agency_id');
+    }
+
+    public function admin(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'admin_id');
+    }
+
+    public function reservations(): HasMany
+    {
+        return $this->hasMany(TourReservation::class, 'check_out_id');
+    }
+}
