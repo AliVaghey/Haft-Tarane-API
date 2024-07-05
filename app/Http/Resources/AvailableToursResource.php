fa78be42d2bcad98a2e9c14db2b7f0aa93f3a930
Ali Vaghey <a.vaghey0@gmail.com>
2024-07-05 19:05:52 +0330

    Implemented the available tours resource

---
+++ app/Http/Resources/AvailableToursResource.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Costs;
+use App\Models\Hotel;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AvailableToursResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $tour = $this->tour;
+        $date = $this->date;
+        $cost = $this->cost;
+        return [
+            'id' => $tour->id,
+            'agency_name' => $tour->agency->name,
+            'title' => $tour->title,
+            'trip_type' => $tour->trip_type,
+            'capacity' => $tour->capacity,
+            'origin' => $tour->origin,
+            'destination' => $tour->destination,
+            'min_cost' => $this->min_cost,
+            'status' => $tour->status,
+            'dates' => [[
+                'id' => $date->id,
+                'start' => $date->start,
+                'end' => $date->end
+            ]],
+            'costs' => $this->filterCosts($cost),
+            'transportation' => null,
+        ];
+    }
+
+    private function filterCosts(Costs $cost_model)
+    {
+        $cost = collect();
+        $c = [
+            'id' => $cost_model->id,
+            'room_type' => $cost_model->room_type,
+            'one_bed' => $cost_model->one_bed,
+            'two_bed' => $cost_model->two_bed,
+            'plus_one' => $cost_model->plus_one,
+            'cld_6' => $cost_model->cld_6,
+            'cld_2' => $cost_model->cld_2,
+            'baby' => $cost_model->baby,
+        ];
+        if ($hotel = Hotel::find($cost_model->hotel_id)) {
+            $c['hotel'] = [
+                'id' => $hotel->id,
+                'name' => $hotel->name,
+                'address' => $hotel->address,
+                'photo' => $hotel->firstPhotoUrl()
+            ];
+        } else {
+            $c['hotel'] = null;
+        }
+        $cost->push($c);
+        return $cost;
+    }
+}
